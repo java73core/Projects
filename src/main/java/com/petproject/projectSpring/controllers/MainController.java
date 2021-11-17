ef3ac2611cd0d43910b0bd2c2066e7e321d605c3
Andrew Mironov <mrvandrey@gmail.com>
2021-11-17 14:57:04 +0400

    Clear empty import

--- src/main/java/com/petproject/projectSpring/controllers/MainController.java
+++ src/main/java/com/petproject/projectSpring/controllers/MainController.java
@@ -1,8 +1,5 @@
 package com.petproject.projectSpring.controllers;
 
-import com.petproject.projectSpring.models.Post;
-import com.petproject.projectSpring.repository.PostRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
