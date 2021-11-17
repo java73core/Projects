57cd726f71e3b11ca97a185b60f6da92f19b2bba
Andrew Mironov <mrvandrey@gmail.com>
2021-11-17 15:08:23 +0400

    OtherController

--- src/main/java/com/petproject/projectSpring/controllers/OtherController.java
+++ src/main/java/com/petproject/projectSpring/controllers/OtherController.java
@@ -1,15 +1,7 @@
-import com.petproject.projectSpring.models.Post;
 import com.petproject.projectSpring.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.Optional;
 
 @Controller
 public class OtherController {
